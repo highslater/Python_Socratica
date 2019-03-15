6872c19bbf3d3efc994dbed9676a440ebb448ea4
highslater <highslater@hotmail.com>
2019-03-15 13:45:07 -0400

    commit

--- python_examples/27_Map_Filter_Reduce.py
+++ python_examples/27_Map_Filter_Reduce.py
@@ -7,6 +7,8 @@ Python Series.
 
 """
 import logging
+from statistics import mean
+from functools import reduce
 
 from my_functions.circles import circle_area
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
@@ -54,4 +56,49 @@ lllll = lambda y: list(map(
     lambda x: round(circle_area(x), y), [x for x in range(0, 11, 1)]))
 print("\n", "*" * 110, "\n")
 print("", lllll(2))
+print("\n", "*" * 110, "\n")
+
+
+temps = [("Berlin", 29), ("Cairo", 36), ("Buenos Aires", 19),
+         ("Los Angeles", 26), ("Tokyo", 27), ("New York", 28),
+         ("London", 22), ("Beijing", 32)]
+
+c_to_f = lambda data: ([data[0], 9 / 5 * data[1] + 32])
+lm = list(map(c_to_f, temps))
+print("\n", "*" * 110, "\n")
+print("", lm)
+print("\n", "*" * 110, "\n")
+
+# ======= Filter Function =======
+
+data = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
+ave = mean(data)
+above_ave = list(filter(lambda x: x > ave, data))
+below_ave = list(filter(lambda x: x < ave, data))
+print("\n", "*" * 110, "\n")
+print("ABOVE:\t\t\t", above_ave)
+print("BELOW:\t\t\t", below_ave)
+print("\n", "*" * 110, "\n")
+
+countries = ["", "Argentina", "", "Brazil", "Chile", "", "Colombia",
+             "", "Ecuador", "", "", "Venezuela"]
+c = list(filter(None, countries))
+print("\n", "*" * 110, "\n")
+print("\t\t", c)
+print("\n", "*" * 110, "\n")
+
+# ======= Reduce Function =======
+
+data = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+multiplier = lambda x, y: x * y
+md = reduce(multiplier, data)
+print("\n", "*" * 110, "\n")
+print("\t\t\t", md)
+print("\n", "*" * 110, "\n")
+
+product = 1
+for x in data:
+    product = product * x
+print("\n", "*" * 110, "\n")
+print("\t\t\t", product)
 print("\n", "*" * 110, "\n")
