582128956a11bb079cde0ea082335d58f83de2c1
highslater <highslater@hotmail.com>
2019-03-16 12:36:57 -0400

    commit

---
+++ python_examples/08_Interactive_Help.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.7
+
+"""08_Interactive_Help.py.
+
+Eighth Program of the Socratica Sexy-Hologram-Human/Computer-Hybrid
+Python Series.
+
+"""
+import logging
+
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(filename="LOG_files/LOG_08.Log",
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
+                    filemode='a')
+logger = logging.getLogger()
+logger.info("08_Interactive_Help.py RUN / START")
+
+
+"""
+
+
+
+
+"""
