6faeba7e8dd62259513fae819890e699332f74d9
highslater <highslater@hotmail.com>
2019-03-16 13:53:20 -0400

    commit

--- python_examples/08_Interactive_Help.py
+++ python_examples/08_Interactive_Help.py
@@ -20,8 +20,57 @@ logger.info("08_Interactive_Help.py RUN / START")
 
 
 """
+>>> dir
+<built-in function dir>
+>>> dir()
+['__builtins__','__doc__','__loader__','__name__','__package__','__spec__']
+>>> dir(__builtins__)
+['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException',
+'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning',
+'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError',
+'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning',
+'EOFError', 'Ellipsis', 'EnvironmentError', 'Exception', 'False',
+'FileExistsError', 'FileNotFoundError', 'FloatingPointError',
+'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning',
+'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError',
+'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'NameError',
+'None', 'NotADirectoryError', 'NotImplemented', 'NotImplementedError',
+'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError',
+'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning',
+'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration',
+SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError',
+'TimeoutError', 'True', 'TypeError', 'UnboundLocalError',
+'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError',
+'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError',
+'Warning', 'ZeroDivisionError', '_', '__build_class__', '__debug__',
+'__doc__', '__import__', '__loader__', '__name__', '__package__', '__spec__',
+'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'bytearray', 'bytes', 'callable',
+'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr',
+'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter',
+'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash',
+'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter',
+'len', 'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next',
+'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range',
+'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted',
+'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
+>>> help(pow)
+Help on built-in function pow in module builtins:
+
+pow(x, y, z=None, /)
+    Equivalent to x**y (with two arguments) or x**y % z (with three arguments)
+
+    Some types, such as ints, are able to use a more efficient algorithm when
+    invoked using the three argument form.
 
 
+>>> help(hex)
+Help on built-in function hex in module builtins:
+
+hex(number, /)
+    Return the hexadecimal representation of an integer.
+
+    >>> hex(12648430)
+    '0xc0ffee'
 
 
 """
