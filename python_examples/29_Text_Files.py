5d9606ae16e4b7355c4b9babc33da586b2c6e904
highslater <highslater@hotmail.com>
2019-03-15 23:20:23 -0400

    commit

--- python_examples/29_Text_Files.py
+++ python_examples/29_Text_Files.py
@@ -17,3 +17,40 @@ logging.basicConfig(filename="LOG_files/LOG_29.Log",
                     filemode='a')
 logger = logging.getLogger()
 logger.info("29_Text_Files.py RUN / START")
+
+# ====== Read Files ======
+
+f = open("./files/help_1.txt")
+text = f.read()
+f.close()
+print(text)
+
+
+with open("./files/help_2.txt") as fobj:
+    text = fobj.read()
+print(text)
+
+
+try:
+    with open("Future_Lottery_Numbers.txt") as f:
+        text = f.read()
+except FileNotFoundError:
+    print("File Not Found, Returning: ", end="")
+    text = None
+print(text)
+
+# ====== Write Files ======
+
+oceans = ["Pacific", "Atlantic", "Indian", "Southern", "Arctic"]
+with open("./files/oceans.txt", 'w') as f:
+    for ocean in oceans:
+        f.write(ocean)
+        f.write("\n")
+
+with open("./files/oceans.txt", 'w') as f:
+    for ocean in oceans:
+        print(ocean, file=f)
+
+with open("./files/oceans.txt", 'a') as f:
+    print(23 * "=", file=f)
+    print("These are the 5 oceans", file=f)
